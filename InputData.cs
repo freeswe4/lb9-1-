89a73e452727cc0951e93697ed7bf0a1a4372a86
Максим Кононов РИС 24-2 <maksimys20066@gmail.com>
2025-02-09 16:41:04 +0300

    Add files via upload

---
+++ InputData.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lb9
+{
+    public static class InputData
+    {
+        static string[] errorMessages = { "Неверный ввод данных. Вводите целые числа", "Неверный ввод. Вводите неотрицательный числа и не превышающего границы" };
+
+        public static bool IntValidate(int num, int min, int max) //провекра ввода на соответствие границ
+        {
+            return num >= min && num <= max;
+        }
+
+        public static int IntInput(string message, string errorMessage) //провекра ввода на соответствие типа входных данных
+        {
+            bool isCorrect;
+            int num;
+            do
+            {
+                Console.WriteLine(message);
+
+                isCorrect = Int32.TryParse(Console.ReadLine(), out num);
+                if (!isCorrect) 
+                    Console.WriteLine(errorMessage);
+            } while (!isCorrect);
+
+            return num;
+        }
+
+        public static int InputAndValidateInt(string message, int min, int max) //провекра ввода на соответствие границам и правильности ввода
+        {
+            bool isCorrect;
+            int num;
+
+            do
+            {
+                Console.WriteLine(message);
+                isCorrect = Int32.TryParse(Console.ReadLine(), out num);
+                if (!isCorrect)
+                    Console.WriteLine(errorMessages[0]);
+                if (!IntValidate(num, min, max))
+                {
+                    Console.WriteLine(errorMessages[1]);
+                    isCorrect = false;
+                }  
+            } while (!isCorrect);
+
+            return num;
+        }
+    }
+}
